75d605298aecb22ca765c5f4c36978e06e468e97
Kunall <146503735+Kunals0612@users.noreply.github.com>
2023-11-29 18:52:26 +0530

    Update Assignment 7.cpp

--- Assignment 7.cpp
+++ Assignment 7.cpp
@@ -6,6 +6,7 @@ public:
     int row;
     int seatnum;
     bool book;
+    int data;
     Seat *next;
     Seat *prev;
     Seat(int r, int s)
@@ -29,6 +30,7 @@ public:
 class Cinemahall
 {
 public:
+    // int matrix[10][7];
     Row *rows[10];
     Cinemahall()
     {
@@ -55,38 +57,58 @@ public:
             }
         }
     }
-    void available()
-    {
-        int matrix[10][7];
-        for (int i = 0; i < 10; i++)
-        {
-            int j = 0;
-            Seat *temp = rows[i]->head;
-            do
-            {
+    // void available()
+    // {
+    //     //int matrix[10][7];
+    //     for (int i = 0; i < 10; i++)
+    //     {
 
-                if (temp->book == false)
-                {
-                    matrix[i][j] = 0;
-                }
-                j++;
-                temp = temp->next;
-            } while (temp != rows[i]->head);
-        }
+    //         Seat *temp = rows[i]->head;
+    //         do
+    //         {
+
+    //             if (temp->book == false)
+    //             {
+
+    //             }
+
+    //             temp = temp->next;
+    //         } while (temp != rows[i]->head);
+    //     }
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     for (int j = 0; j < 7; j++)
+    //     {
+    //         cout << matrix[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // }
+    void display()
+    {
+
         for (int i = 0; i < 10; i++)
         {
+            Seat *temp = rows[i]->head;
             for (int j = 0; j < 7; j++)
             {
-                cout << matrix[i][j] << " ";
+                if(temp->book == true)
+                {
+                    cout<<"(B)";
+                }
+                else{
+                    cout<<"(_)";
+                }
+                temp = temp->next;
             }
             cout << endl;
         }
     }
     void bookseat(int r, int s)
     {
-        int matrix[10][7];
+        // int matrix[10][7];
         Seat *temp = rows[r]->head;
-        int j = 0;
+
         do
         {
             if (temp->seatnum == s && temp->book == false)
@@ -94,42 +116,70 @@ public:
                 temp->book = true;
                 return;
             }
-            if (temp->book == true)
-            {
-                matrix[r][s] = 1;
-            }
-            j++;
             temp = temp->next;
 
         } while (temp != rows[r]->head);
     }
     void cancel(int r, int s)
     {
-        int matrix[10][7];
-        int j = 0;
+        // int matrix[10][7];
+
         Seat *temp = rows[r]->head;
         do
         {
             if (temp->seatnum == s && temp->book == true)
             {
                 temp->book = false;
+                return;
             }
-            if (temp->book == false)
-            {
-                matrix[r][s] = 0;
-            }
+
             temp = temp->next;
-            j++;
+
         } while (temp != rows[r]->head);
     }
 };
 int main()
 {
     Cinemahall cn;
-    cn.bookseat(0, 0);
-    cn.bookseat(0, 3);
-    cout << "Available seats : " << endl;
-    cn.available();
+    cout << "1.Display Available seat......" << endl;
+    cout << "2.Book seat ...." << endl;
+    cout << "3.Cancel seat...." << endl;
+    while (true)
+    {
+        int choice;
+        cout<<endl;
+        cout << "Enter the choice :   ";
+        cin >> choice;
+        if (choice == 1)
+        {
+            cn.display();
+        }
+        else if (choice == 2)
+        {
+            int r, s;
+            cout << endl;
+            cout << "Enter the row number to book the seat";
+            cin >> r;
+            cout << "Enter the seat number to book the seat";
+            cin >> s;
+            cn.bookseat(r, s);
+        }
+        else if (choice == 3)
+        {
+            int r, s;
+            cout << endl;
+            cout << "Enter the row number to cancel the seat";
+            cin >> r;
+            cout << "Enter the seat number to cancel the seat";
+            cin >> s;
+            cn.cancel(r, s);
+        }
+        else
+        {
+            cout << endl;
+            cout << "Invalid Input" << endl;
+        }
+    }
 
     return 0;
 }
